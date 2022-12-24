07f64c03cf499c7b4e94740f3426e1099a75ef68
hoholms <ninikabug@gmail.com>
2022-12-24 17:28:34 +0200

    First commit

---
+++ src/main/java/com/hoholms/onlinewalletapi/OnlineWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoholms.onlinewalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineWalletApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoholms/onlinewalletapi/OnlineWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoholms.onlinewalletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
